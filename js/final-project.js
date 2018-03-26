85ec37eace00997b1a126da705e9e3847d0336f6
Donderstal <daanonderstal@hotmail.com>
2018-03-26 09:28:16 +0200

    Eerste versie - werkende knoppen

--- js/final-project.js
+++ js/final-project.js
@@ -1,57 +1,95 @@
-(function() {
-  // TODO: load CV tab
+/* ----INHOUDSOPGAVE ---- */
+      //\\ Categorie A// VARIABELEN, ARRAYS, OBJECTEN //
+          // A-I \\ // Variabelen
 
-  // TODO: load portfolio tab
+      //\\ Categorie B// FUNCITES //
+          // B-I \\ // Generieke knop-klikker functie
+          // B-II \\ // Creert knoppen
 
-  // EVENT LISTENERS
-  const tabSwitchAnimationDuration = 750
-  document.addEventListener("DOMContentLoaded", function() {
-    $("[data-toggle='tooltip']").tooltip()
+      //\\ Categorie C // EVENT LISTENERS //
+          // C-I \\ // Informatie-knoppen
 
-    // scrollspy
-    $("body").scrollspy({target: "#spy"})
 
-    $(".cv").show(tabSwitchAnimationDuration, function() {
-      $(this).animate({opacity: 1.0})
-    })
-  })
 
-  const cvTab = document.getElementById("tab-cv")
-  const portfolioTab = document.getElementById("tab-portfolio")
 
-  const tabListener = event => {
-    const toShowClass = event.target.id === "tab-cv"
-      ? "cv"
-      : "portfolio"
+//\\ Categorie A// VARIABELEN, ARRAYS, OBJECTEN //
 
-    const toHideClass = event.target.id === "tab-cv"
-      ? "portfolio"
-      : "cv"
+// A-I \\ 
+// Variabelen
+const btnAbout = document.getElementById("btnAbout")
 
-    $(`.${toHideClass}`)
-      .animate({opacity: 0.0})
-      .hide(tabSwitchAnimationDuration, function() {
-        $(`.${toShowClass}`).show(tabSwitchAnimationDuration, function() {
-          $(this).animate({opacity: 1.0})
-      })
-    })
+const textAbout = document.getElementById("textAbout")
+
+let btnList = ["About", "Experience", "Core Qualities", "Education", "Certificates", "Skills", "Interests", "Profiles", "Contact"]
+
+let objectAbout = {
+  visibility: false,
+  text: "Daan Onderstal lijkt soms een eikel, maar is eigenlijk een hele lieve jongen. Hij houdt van knuffelen en broodjes kroket. De legende gaat dat hij soms wat zinnigs te melden heeft, maar hier is nog geen onafhankelijk onderzoek naar gedaan.",
+
+}
+
+//\\ Categorie B// FUNCITES //
+
+// B-I \\ 
+// Generieke knop-klikker functie
+function btnClicker(object, location) {
+  if (object.visibility === false) {
+    location.innerHTML = object.text
+    object.visibility = true
   }
-
-  cvTab.addEventListener("click", tabListener)
-  portfolioTab.addEventListener("click", tabListener)
-
-  for (let tab of document.getElementsByClassName("tab")) {
-    tab.addEventListener("click", event => {
-      const element = event.target
-  
-      if (!element.classList.contains("active")) {
-        Array
-          .from(document.getElementsByClassName("tab active"))
-          .forEach(t => t.classList.remove("active"))
-        
-  
-      element.classList.add("active")
-      }
-    })
+  else {
+    location.innerHTML =  ""
+    object.visibility = false
   }
-})()
+}
+
+// B-II \\ 
+// Creert divs adhv btnList
+function divCreater(btnList) {
+  for (i = 0; i < btnList.length; i++) {
+    let nwDiv = document.createElement('div')
+    nwDiv.id = "div"
+    let Lctn = document.getElementsByClassName('main-container')[0]
+    Lctn.appendChild(nwDiv)
+  }
+}
+
+divCreater(btnList)
+
+// B-III \\ 
+// Creert knoppen adhv btnList
+function btnCreater(btnList) {
+  for (i = 0; i < btnList.length; i++) {
+    let nwBtn = document.createElement('button')
+    nwBtn.id = "btn"
+    nwBtn.className = "btn btn-dark"
+    nwBtn.innerHTML = "Werkt dit?"
+    let Lctn = document.getElementsByClassName('main-container')[0]
+    Lctn.appendChild(nwBtn)
+  }
+}
+
+btnCreater(btnList)
+
+// B-IV \\ 
+// Creert divs adhv btnList
+function txtDivCreater(btnList) {
+  for (i = 0; i < btnList.length; i++) {
+    let nwTxtDiv = document.createElement('div')
+    nwTxtDiv.id = 'text-' + btnList[i]
+    nwTxtDiv.className = "container"
+    nwTxtDiv.innerHTML = "Werkt dit?"
+    let Lctn = document.getElementsByClassName('main-container')[0]
+    Lctn.appendChild(nwTxtDiv)
+  }
+}
+
+txtDivCreater(btnList)
+
+//\\ Categorie C // EVENT LISTENERS //
+
+// C-I \\ 
+// Informatie-knoppen
+btnAbout.addEventListener("click", function() {
+  btnClicker(objectAbout, textAbout)
+})
